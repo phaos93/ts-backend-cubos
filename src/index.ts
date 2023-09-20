b32bc919b26fe4373fa00d850cca0dc342b040c0
Pedro Silva <phaos93@gmail.com>
2023-09-20 16:02:43 -0300

    Criando e configurando o arquivo de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
+import rotas from './rotas';
 
 const app = express();
 
 app.use(express.json());
+app.use(rotas);
 
 app.listen('3000', () => {
     console.log('servidor inicializado')
