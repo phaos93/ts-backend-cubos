c7050bf5cf9b7f7ef59692e2748a4ec4c7f98c1c
Pedro Silva <phaos93@gmail.com>
2023-09-21 12:51:13 -0300

    configurando listagem de carros no controlador

--- src/controladores/carros.ts
+++ src/controladores/carros.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
+import { knex } from '../../bancodedados/conexao'
 
-export const listarCarros = async (req: Request, res: Response) => {
-    
+export const listarCarros = async (_: Request, res: Response) => {
+    try {
+        const carros = await knex('carros');
+        return res.json(carros);
+    } catch {
+        return res.status(500).json({ mensagem: 'erro interno do servidor' });
+    }
 }
 
 export const detalharCarros = async (req: Request, res: Response) => {
