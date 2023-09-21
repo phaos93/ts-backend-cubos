74dd4b149bf3008a802d120b05167910ef58374b
Pedro Silva <phaos93@gmail.com>
2023-09-21 13:00:59 -0300

    criaçãi do tipo carro e implementação do endpoint para detalhamento de carro

--- src/controladores/carros.ts
+++ src/controladores/carros.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import { knex } from '../../bancodedados/conexao'
+import { Carro } from '../tipos'
 
 export const listarCarros = async (_: Request, res: Response) => {
     try {
-        const carros = await knex('carros');
+        const carros = await knex<Carro>('carros');
         return res.json(carros);
     } catch {
         return res.status(500).json({ mensagem: 'erro interno do servidor' });
@@ -11,7 +12,20 @@ export const listarCarros = async (_: Request, res: Response) => {
 }
 
 export const detalharCarros = async (req: Request, res: Response) => {
-    
+    const { id } = req.params;
+        try {
+            const carro = await knex<Carro>('carros')
+                .where({ id: Number(id) })
+                .first();
+            
+            if (!carro) {
+                return res.status(404).json({ mensagem: 'carro não encontrado' });
+            }
+            
+            return res.json(carro);
+    } catch {
+        return res.status(500).json({ mensagem: 'erro interno do servidor' });
+    }
 }
 
 export const cadastrarCarros = async (req: Request, res: Response) => {
